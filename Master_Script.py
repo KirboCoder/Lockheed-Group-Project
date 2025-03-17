fc9d3cf8b40cd6d863f70ddde9cdae4b65da5fd8
Michael <147848602+KirboCoder@users.noreply.github.com>
2025-03-17 17:17:19 -0400

    Fix with pip install and check

--- Master_Script.py
+++ Master_Script.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import sys
 
 def run_script(script_path):
     """Run a Python script using subprocess."""
@@ -9,8 +10,33 @@ def run_script(script_path):
     except subprocess.CalledProcessError as e:
         print(f"Error running {script_path}: {e}")
 
+def check_and_install_packages(packages):
+    """Check if packages are installed and install them if not."""
+    for package in packages:
+        try:
+            # Try to import the package
+            __import__(package)
+        except ImportError:
+            # If the package is not found, install it
+                print(f"Installing {package} locally...")
+                subprocess.run(['pip', 'install', package])
+
+# List of packages to check and install
+packages_to_check = [
+    'transformers',
+    'torch',
+    'nltk',
+    'beautifulsoup4',
+    'pandas',
+    'numpy',
+    'requests'
+]
+
 def main():
+    check_and_install_packages(packages_to_check)
+
     # Paths to the scripts
+    # To disable a script, you must add a '#' at the begining of the line.
     script1_path = os.path.join(os.getcwd(), 'Script.py')
     script2_path = os.path.join(os.getcwd(), 'Country-Maps-script.py')
     script3_path = os.path.join(os.getcwd(), 'GFP.py')
